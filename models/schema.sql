0b2802c85f5f49b4970e764a8d081b49d45efc62
Alexiss Diaz <alexiss.diaz@live.com>
2020-01-04 10:38:04 -0600

    added schema and tried to clean up the files abit

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,134 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+-- DROP DATABASE IF EXISTS ra49m4zmt16g6ssl;
+-- CREATE DATABASE ra49m4zmt16g6ssl;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS test_ra49m4zmt16g6ssl;
+CREATE DATABASE test_ra49m4zmt16g6ssl;
+
+
+CREATE TABLE owners
+(
+    owner_id INT
+    AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR
+    (30) NOT NULL,
+    last_name VARCHAR
+    (30) NOT NULL,
+    email VARCHAR
+    (50) NOT NULL,
+    phone_number VARCHAR
+    (15) NOT NULL,
+    PRIMARY KEY
+    (owner_id) ON
+    DELETE CASCADE
+);
+
+    CREATE TABLE pets
+    (
+        pets_id INT
+        AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    pets_name VARCHAR
+        (30) NOT NULL,
+    pet_type VARCHAR
+        (30) NOT NULL,
+    pet_weight INT NOT NULL,
+    pet_birthday VARCHAR
+        (30) NOT NULL,
+    pet_gender VARCHAR
+        (30) NOT NULL,
+    pet_personality VARCHAR
+        (30) NOT NULL,
+    PRIMARY KEY
+        (pets_id),
+    FOREIGN KEY
+        (owner_id) REFERENCES owners
+        (owner_id) ON
+        DELETE CASCADE
+);
+
+        CREATE TABLE pet_feeding
+        (
+            pet_info INT
+            AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT NOT NULL,
+    pet_food VARCHAR
+            (30) NOT NULL,
+    morining_feeding TIME,
+    afternoon_feeding TIME,
+    night_feeding TIME,
+    PRIMARY KEY
+            (pet_info),
+    FOREIGN KEY
+            (pet_id) REFERENCES pets
+            (pet_id)
+);
+
+            CREATE TABLE pet_vaccinations
+            (
+                pet_vac INT
+                AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT NOT NULL,
+    vac_name VARCHAR
+                (30) NOT NULL,
+    vac_status VARCHAR
+                (30) NOT NULL,
+    vac_due_date TIME,
+    night_feeding TIME,
+    PRIMARY KEY
+                (pet_vac),
+    FOREIGN KEY
+                (pet_id) REFERENCES pet_vaccinations
+                (pet_id)
+);
+
+                CREATE TABLE medication
+                (
+                    med_id INT
+                    AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT NOT NULL,
+    needs_meds BOOLEAN DEFAULT false,
+    medication_name VARCHAR
+                    (40),
+    medication_time VARCHAR
+                    (40),
+    dosage VARCHAR
+                    (40),
+    PRIMARY KEY
+                    (med_id),
+    FOREIGN KEY
+                    (pet_id) REFERENCES pets
+                    (pets_id) ON
+                    DELETE CASCADE
+);
+
+                    CREATE TABLE appointments
+                    (
+                        appt_id INT
+                        AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT NOT NULL,
+    vet_name VARCHAR
+                        (40) NOT NULL,
+    vet_address VARCHAR
+                        (40) NOT NULL,
+    appt DATETIME,
+    PRIMARY KEY
+                        (appt_id),
+    FOREIGN KEY
+                        (pet_id) REFERENCES pets
+                        (pet_id)
+);
+
+                        CREATE TABLE special_instructions
+                        (
+                            instruction_id INT
+                            AUTO_INCREMENT PRIMARY KEY,
+    pet_id INT NOT NULL,
+    instruction BOOLEAN DEFAULT false,
+    info VARCHAR
+                            (150) NOT NULL,
+    PRIMARY KEY
+                            (instruction_id),
+    FOREIGN KEY
+                            (pet_id) REFERENCES pets
+                            (pet_id)
+);
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
@@ -12,14 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
@@ -34,8 +25,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
