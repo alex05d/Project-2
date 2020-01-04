6b31b47d2704ab64ecc78a7136e3d0e009fc238b
Alexiss Diaz <alexiss.diaz@live.com>
2020-01-04 11:58:19 -0600

    added the longing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,40 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
+
+  //login route
+  app.get("/", (req, res) => {
+    res.render('login.ejs')
+  })
+
+  //login POST method
+  app.get('/login', (req, res) => {
+    res.render('login.ejs')
+  })
+
+  //register route
+  app.get('/register', (req, res) => {
+    res.render('register.ejs')
+  })
+
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // // Delete an example by id
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
