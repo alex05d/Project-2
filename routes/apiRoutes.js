d864ac705c6859f8ec3e6000e3ae1c72319d5b6c
Kate Davis <katedavis@Kates-MacBook-Pro.local>
2020-01-11 13:28:35 -0600

    minor changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,43 +23,43 @@ var cloudinary = require('cloudinary')
 //     res.render('register.ejs')
 //   })
 
-  // Get all examples
-  // app.get("/api/examples", function (req, res) {
-  //   db.Example.findAll({}).then(function (dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
+// Get all examples
+// app.get("/api/examples", function (req, res) {
+//   db.Example.findAll({}).then(function (dbExamples) {
+//     res.json(dbExamples);
+//   });
+// });
 
-  // // Create a new example\
+// // Create a new example\
 
-  app.post("/api/pet_image", function(req, res) {
-    // db.Example.create(req.body).then(function(dbExample) {
-    //   res.json(dbExample);
-    // });
-    // console.log(req.body);
-    var dataURI = req.body.dataURI;
-    var uploadStr = 'data:image/jpeg;base64,' + dataURI;
+// app.post("/api/pet_image", function(req, res) {
+// db.Example.create(req.body).then(function(dbExample) {
+//   res.json(dbExample);
+// });
+// console.log(req.body);
+// var dataURI = req.body.dataURI;
+// var uploadStr = 'data:image/jpeg;base64,' + dataURI;
+//
+// cloudinary.v2.uploader.upload(uploadStr, {
+// api_key: "773177291336529",
+// api_secret: "FZUKToWuQ9ijBcUCygsLd7rXvCg",
+// cloud_name: "katedavis",
+//   overwrite: true,
+//  invalidate: true,
+// width: 810, height: 456, crop: "fill"
+// },
+// function (error, result) {
+// res.json(result);
+//  });
+// res.json(req.body)
+//});
 
-    cloudinary.v2.uploader.upload(uploadStr, {
-      api_key: "773177291336529",
-      api_secret: "FZUKToWuQ9ijBcUCygsLd7rXvCg",
-      cloud_name: "katedavis",
-        overwrite: true,
-        invalidate: true,
-        width: 810, height: 456, crop: "fill"
-    },
-        function (error, result) {
-            res.json(result);
-        });
-    // res.json(req.body)
-  });
-
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+// // Delete an example by id
+// app.delete("/api/examples/:id", function(req, res) {
+//   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//     res.json(dbExample);
+//   });
+// });
 // };
 module.exports = function (app) {
 
@@ -109,7 +109,27 @@ module.exports = function (app) {
     })
 
   })
+  app.post("/api/pet_image", function (req, res) {
+    // db.Example.create(req.body).then(function(dbExample) {
+    //   res.json(dbExample);
+    // });
+    // console.log(req.body);
+    var dataURI = req.body.dataURI;
+    var uploadStr = 'data:image/jpeg;base64,' + dataURI;
 
+    cloudinary.v2.uploader.upload(uploadStr, {
+      api_key: "773177291336529",
+      api_secret: "FZUKToWuQ9ijBcUCygsLd7rXvCg",
+      cloud_name: "katedavis",
+      overwrite: true,
+      invalidate: true,
+      width: 810, height: 456, crop: "fill"
+    },
+      function (error, result) {
+        res.json(result);
+      });
+    // res.json(req.body)
+  });
 };
 
 
